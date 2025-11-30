a173b66d41f7ae830457ce1256361c6ce56fb170
Artur Jozefiak <jozefiak@secra.de>
2025-11-30 09:14:40 +0000

    Refactor `Plugin.php` in Contao Slider CAPTCHA bundle for improved code clarity and type safety

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -1,10 +1,20 @@
 <?php
+
 namespace Solidwork\ContaoSliderCaptchaBundle\ContaoManager;
+
 use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
 use Solidwork\ContaoSliderCaptchaBundle\SolidworkContaoSliderCaptchaBundle;
-class Plugin implements BundlePluginInterface{
- public function getBundles(array $bundles){
-  return [BundleConfig::create(SolidworkContaoSliderCaptchaBundle::class)->setLoadAfter([ContaoCoreBundle::class])];
- }}
+
+class Plugin implements BundlePluginInterface
+{
+    public function getBundles(ParserInterface $parser): array
+    {
+        return [
+            BundleConfig::create(SolidworkContaoSliderCaptchaBundle::class)
+                ->setLoadAfter([ContaoCoreBundle::class]),
+        ];
+    }
+}
